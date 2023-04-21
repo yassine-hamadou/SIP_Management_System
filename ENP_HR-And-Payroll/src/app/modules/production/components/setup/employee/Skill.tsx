d924110a5d1d7e5618d413a5dc65455c9c0508e7
kofi-k <kofivincent026@gmail.com>
2023-04-21 19:45:04 +0000

    apply setup component on hr setups

--- ENP_HR-And-Payroll/src/app/modules/production/components/setup/employee/Skill.tsx
+++ ENP_HR-And-Payroll/src/app/modules/production/components/setup/employee/Skill.tsx
@@ -1,10 +1,3 @@
-import { Button, Input, Modal, Space, Table } from 'antd'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { KTCardBody, KTSVG } from '../../../../../../_metronic/helpers'
-import { Api_Endpoint, updateSkill } from '../../../../../services/ApiCalls'
-import { useQueryClient, useMutation } from 'react-query'
 import { SetupComponent } from '../CommonSetupComponent'
 
 const Skill = () => {
