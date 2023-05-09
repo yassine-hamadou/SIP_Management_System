f24afa2b7398238711a9e46b8e0cda00de07b47c
Ahercode <philipkelly407@gmail.com>
2023-05-09 14:29:36 +0000

    fix bug on topbar

--- ENP_Production/src/app/modules/auth/core/_models.ts
+++ ENP_Production/src/app/modules/auth/core/_models.ts
@@ -48,8 +48,8 @@ export interface UserModel {
   username: string
   password: string | undefined
   email: string
-  first_name: string
-  last_name: string
+  firstName: string
+  surname: string
   fullname?: string
   occupation?: string
   companyName?: string
