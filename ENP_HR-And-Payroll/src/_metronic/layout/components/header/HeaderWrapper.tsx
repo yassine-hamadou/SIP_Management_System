6d865224f99732c0c53075527973f94754edbe63
kofi-k <kofivincent026@gmail.com>
2023-06-16 19:03:32 +0000

    merge with upstream

--- ENP_HR-And-Payroll/src/_metronic/layout/components/header/HeaderWrapper.tsx
+++ ENP_HR-And-Payroll/src/_metronic/layout/components/header/HeaderWrapper.tsx
@@ -40,13 +40,11 @@ export function HeaderWrapper() {
         {!aside.display && (
           <div className='d-flex align-items-center flex-grow-1 flex-lg-grow-0'>
             <Link to='/dashboard' className='d-lg-none'>
-<<<<<<< HEAD
               <h3 style={{color:"#009EF7"}}>HR - PAYROLL</h3>
               {/* <img alt='Logo' src={toAbsoluteUrl('/media/logos/default-small.svg')} className='h-30px' /> */}
 =======
               {/* <img alt='Logo' src={toAbsoluteUrl('/media/logos/default-small.svg')} className='h-30px' /> */}
               <h3 style={{color: "#009EF7"}}>HR - PAYROLL</h3>
->>>>>>> a4fe16c18c945cf23eb9275aaa23fc5e251899d1
             </Link>
           </div>
         )}
@@ -55,14 +53,14 @@ export function HeaderWrapper() {
         {aside.display && (
           <div className='d-flex align-items-center flex-grow-1 flex-lg-grow-0'>
             <Link to='/' className='d-lg-none'>
-<<<<<<< HEAD
+
             <h3 style={{color:"#009EF7"}}>HR - PAYROLL</h3>
               {/* <img alt='Logo' src={toAbsoluteUrl('/media/logos/default-small.svg')} className='h-30px' /> */}
-=======
+
               {/* <img alt='Logo' src={toAbsoluteUrl('/media/logos/default-small.svg')} className='h-30px' /> */}
               <h3 style={{color: "#009EF7"}}>HR - PAYROLL</h3> 
 
->>>>>>> a4fe16c18c945cf23eb9275aaa23fc5e251899d1
+
             </Link>
           </div>
         )}
